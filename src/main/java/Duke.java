ec6b951d909e9ded159ba7f20479714fdc91e5bc
lhydl <dereklee9262@gmail.com>
2020-08-23 11:00:35 +0800

    Implement greet, echo and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,9 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?\n");
+
+        String line = "";
+        Scanner in = new Scanner(System.in);
+
+        while(!line.equals("bye")){
+            line = in.nextLine();
+            System.out.println(line + "\n");
+        }
+
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
