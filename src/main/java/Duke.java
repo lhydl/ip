18fc2baebdb77e198363748b0602b982734573c4
lhydl <dereklee9262@gmail.com>
2020-08-23 14:42:06 +0800

    Implement mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,25 +12,33 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
 
-        String[] arr = new String[100];
         Scanner in = new Scanner(System.in);
+        Task[] tasks = new Task[100];
 
         int count = 0;
         int i, j;
 
-        for(i=0; i<arr.length; i++){
-            String s = in.nextLine();
-            if(!s.equals("list") && !s.equals("bye")) {
-                arr[i] = s;
-                System.out.println("Added: " + arr[i] + "\n");
+        for(i=0; i<tasks.length; i++) {
+            String str = in.nextLine();
+            if(!str.equals("list") && !str.equals("bye") && !str.contains("done")) {
+                tasks[count] = new Task(str);
+                System.out.println("Added: " + tasks[i].getDisplayString() + "\n");
                 count++;
             }
-            else if(s.equals("list")){
+            else if(str.equals("list")) {
                 i--;
+                System.out.println("Here are the tasks in your list:");
                 for(j=1; j<=count; j++) {
-                    System.out.println(j + ". " + arr[j-1] );
+                    System.out.println(j + ". " + "[" + tasks[j-1].getStatusIcon() + "] " + tasks[j-1].getDisplayString() );
                 }
                 System.out.println(); //added a new line after the list
+            }
+            else if(str.contains("done")) {
+                String digit = str.replaceAll("[^0-9]", ""); //extract digit from a string
+                int num = Integer.parseInt(digit); //change string to int
+                tasks[num-1].setAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks[num-1].getStatusIcon() + "] " + tasks[num-1].getDisplayString() + "\n" );
             }
             else {
                 break;
